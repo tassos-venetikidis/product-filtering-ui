8089fd880702d8a9b1e8b9dbbe6b9a06466f61e0
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-12 22:47:36 +0200

    Added functionality to display the product objects from the products array to the DOM

--- script.js
+++ script.js
@@ -1,74 +1,109 @@
 const products = [
   {
-    name: 'Sony Playstation 5',
-    url: 'images/playstation_5.png',
-    type: 'games',
+    name: "Sony Playstation 5",
+    url: "images/playstation_5.png",
+    type: "games",
     price: 499.99,
   },
   {
-    name: 'Samsung Galaxy',
-    url: 'images/samsung_galaxy.png',
-    type: 'smartphones',
+    name: "Samsung Galaxy",
+    url: "images/samsung_galaxy.png",
+    type: "smartphones",
     price: 399.99,
   },
   {
-    name: 'Cannon EOS Camera',
-    url: 'images/cannon_eos_camera.png',
-    type: 'cameras',
+    name: "Cannon EOS Camera",
+    url: "images/cannon_eos_camera.png",
+    type: "cameras",
     price: 749.99,
   },
   {
-    name: 'Sony A7 Camera',
-    url: 'images/sony_a7_camera.png',
-    type: 'cameras',
+    name: "Sony A7 Camera",
+    url: "images/sony_a7_camera.png",
+    type: "cameras",
     price: 1999.99,
   },
   {
-    name: 'LG TV',
-    url: 'images/lg_tv.png',
-    type: 'televisions',
+    name: "LG TV",
+    url: "images/lg_tv.png",
+    type: "televisions",
     price: 799.99,
   },
   {
-    name: 'Nintendo Switch',
-    url: 'images/nintendo_switch.png',
-    type: 'games',
+    name: "Nintendo Switch",
+    url: "images/nintendo_switch.png",
+    type: "games",
     price: 299.99,
   },
   {
-    name: 'Xbox Series X',
-    url: 'images/xbox_series_x.png',
-    type: 'games',
+    name: "Xbox Series X",
+    url: "images/xbox_series_x.png",
+    type: "games",
     price: 499.99,
   },
   {
-    name: 'Samsung TV',
-    url: 'images/samsung_tv.png',
-    type: 'televisions',
+    name: "Samsung TV",
+    url: "images/samsung_tv.png",
+    type: "televisions",
     price: 1099.99,
   },
   {
-    name: 'Google Pixel',
-    url: 'images/google_pixel.png',
-    type: 'smartphones',
+    name: "Google Pixel",
+    url: "images/google_pixel.png",
+    type: "smartphones",
     price: 499.99,
   },
   {
-    name: 'Sony ZV1F Camera',
-    url: 'images/sony_zv1f_camera.png',
-    type: 'cameras',
+    name: "Sony ZV1F Camera",
+    url: "images/sony_zv1f_camera.png",
+    type: "cameras",
     price: 799.99,
   },
   {
-    name: 'Toshiba TV',
-    url: 'images/toshiba_tv.png',
-    type: 'televisions',
+    name: "Toshiba TV",
+    url: "images/toshiba_tv.png",
+    type: "televisions",
     price: 499.99,
   },
   {
-    name: 'iPhone 14',
-    url: 'images/iphone_14.png',
-    type: 'smartphones',
+    name: "iPhone 14",
+    url: "images/iphone_14.png",
+    type: "smartphones",
     price: 999.99,
   },
 ];
+
+const searchInput = document.getElementById("search");
+const cartBtn = document.getElementById("cart-button");
+const filtersContainer = document.getElementById("filters-container");
+const camerasCheck = document.getElementById("cameras");
+const smartphonesCheck = document.getElementById("smartphones");
+const gamesCheck = document.getElementById("games");
+const televisionsCheck = document.getElementById("televisions");
+const productsWrapper = document.getElementById("products-wrapper");
+
+function displayProductToDOM(product) {
+  const div = document.createElement("div");
+  div.className = "item space-y-2";
+  div.innerHTML = `
+    <div
+            class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border"
+          >
+            <img
+              src="${product.url}"
+              alt="${product.name}"
+              class="w-full h-full object-cover"
+            />
+            <span
+              class="status bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0"
+              >Add To Cart</span
+            >
+          </div>
+          <p class="text-xl">${product.name}</p>
+          <strong>$${product.price}</strong>
+  `;
+
+  productsWrapper.append(div);
+}
+
+products.forEach((product) => displayProductToDOM(product));

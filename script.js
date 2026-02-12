ed9b079c6df05a8125d4da48752c117f729867e1
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2026-02-12 13:53:33 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    name: 'Sony Playstation 5',
+    url: 'images/playstation_5.png',
+    type: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung Galaxy',
+    url: 'images/samsung_galaxy.png',
+    type: 'smartphones',
+    price: 399.99,
+  },
+  {
+    name: 'Cannon EOS Camera',
+    url: 'images/cannon_eos_camera.png',
+    type: 'cameras',
+    price: 749.99,
+  },
+  {
+    name: 'Sony A7 Camera',
+    url: 'images/sony_a7_camera.png',
+    type: 'cameras',
+    price: 1999.99,
+  },
+  {
+    name: 'LG TV',
+    url: 'images/lg_tv.png',
+    type: 'televisions',
+    price: 799.99,
+  },
+  {
+    name: 'Nintendo Switch',
+    url: 'images/nintendo_switch.png',
+    type: 'games',
+    price: 299.99,
+  },
+  {
+    name: 'Xbox Series X',
+    url: 'images/xbox_series_x.png',
+    type: 'games',
+    price: 499.99,
+  },
+  {
+    name: 'Samsung TV',
+    url: 'images/samsung_tv.png',
+    type: 'televisions',
+    price: 1099.99,
+  },
+  {
+    name: 'Google Pixel',
+    url: 'images/google_pixel.png',
+    type: 'smartphones',
+    price: 499.99,
+  },
+  {
+    name: 'Sony ZV1F Camera',
+    url: 'images/sony_zv1f_camera.png',
+    type: 'cameras',
+    price: 799.99,
+  },
+  {
+    name: 'Toshiba TV',
+    url: 'images/toshiba_tv.png',
+    type: 'televisions',
+    price: 499.99,
+  },
+  {
+    name: 'iPhone 14',
+    url: 'images/iphone_14.png',
+    type: 'smartphones',
+    price: 999.99,
+  },
+];
